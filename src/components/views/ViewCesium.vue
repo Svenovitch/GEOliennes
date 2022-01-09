93b4a0578c88dbb0bab7fdef4eb2933621dc34b4
Remi Girard <remi.girard@hes-so.ch>
2022-01-09 16:41:14 +0100

    model eolienne fonctionne

--- src/components/views/ViewCesium.vue
+++ src/components/views/ViewCesium.vue
@@ -13,7 +13,20 @@ export default {
     return{
       center: [7.07903, 46.68856],
       defaultheight:10000.,
-      viewer:null
+      viewer:null,
+      locations : [
+        ["Gibloux1", 46.67679, 7.02146],
+        ["Gibloux2", 46.66365, 7.00686],
+        ["Glaney1", 46.67890, 6.87359],
+        ["Glaney2", 46.65247, 6.84562],
+        ["Vuisternens", 46.62848, 6.90941],
+        ["Esserta", 46.61058, 6.94127],
+        ["Schwyberg1", 46.68031, 7.25892],
+        ["Schwyberg2", 46.69877, 7.26710],
+        ["Surpierre-Cheiry", 46.74431, 6.84192],
+        ["Sonnaz1", 46.84741, 7.11054],
+        ["Sonnaz2", 46.83535, 7.09383]
+      ],
     }
   },
   methods: {
@@ -48,11 +61,10 @@ export default {
       let posCam = viewer.scene.camera.positionCartographic
       let latCam = posCam.latitude
       let longCam = posCam.longitude
-      Cesium.sampleTerrain(viewer.terrainProvider, 9, [posCam])
+      Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, [posCam])
       .then(function(samples) {
         let groundHeight = samples[0].height;
-        var destHeight = groundHeight + 2
-        console.log(destHeight)
+        var destHeight = groundHeight + 2;
         viewer.camera.flyTo({
           destination : Cesium.Cartesian3.fromRadians(longCam, latCam, destHeight),
           orientation: {
@@ -62,6 +74,50 @@ export default {
           },
         });
       });
+    },
+
+    // Affiche les éoliennes
+    displayEolienne(){
+      var viewer = this.viewer
+      var uri
+      var promise = Cesium.IonResource.fromAssetId(752612)
+      .then(function (resource) {
+        uri = resource
+      });
+
+      for (var i = 0; i < this.locations.length; i++) {
+        let position = Cesium.Cartographic.fromDegrees(this.locations[i][2],this.locations[i][1])
+        let location = Cesium.Cartesian3.fromDegrees(this.locations[i][2],this.locations[i][1])
+        Cesium.sampleTerrainMostDetailed(this.viewer.terrainProvider, [position])
+        .then(function(samples) {
+          let groundHeight = samples[0].height;
+          viewer.entities.add({
+            position: Cesium.Cartesian3.fromRadians(position.longitude,position.latitude, groundHeight),
+            model : {
+              uri: uri,
+              //gltf : '../../assets/scene.glb',
+              //heightReference : Cesium.HeightReference.CLAMP_TO_GROUND,
+              scale : 15,
+              color : Cesium.Color.WHITE,
+            },
+//            ellipse: {
+//              semiMinorAxis: 5,
+//              semiMajorAxis: 5,
+//              height: groundHeight,
+//              extrudedHeight: groundHeight + 90,
+//              material: Cesium.Color.AQUA,
+//            },
+          });
+          viewer.entities.add({
+            position: location,
+            ellipse: {
+              semiMinorAxis: 30,
+              semiMajorAxis: 30,
+              material: Cesium.Color.AQUA,
+            },
+          });
+        });
+      };
     },
 
     /**
@@ -98,6 +154,7 @@ export default {
         selectionIndicator: false,
         timeline: false,
         animation: false,
+        shouldAnimate: true,
         geocoder: true,
         navigationInstructionsInitiallyVisible: false,
         navigationHelpButton: false,
@@ -117,52 +174,16 @@ export default {
     Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
 
     this.viewer = this.setupCesiumGlobe();
-    // Transparence du MNT, décommenter pour defaire le calcul de profondeur
+    // Transparence du MNT, décommenter pour faire le calcul de profondeur
     this.viewer.scene.globe.depthTestAgainstTerrain = true;
     
     // Ajoute Swissbuilding et SwissTLM
     this.viewer.scene.primitives.add(this.getSwissBuilding());
     this.viewer.scene.primitives.add(this.getSwissTLM());
 
-    // Ajoute les eoliennes -- à simplifier avec methode
-    var positions = [
-      Cesium.Cartesian3.fromDegrees(7.02146, 46.67679),
-      Cesium.Cartesian3.fromDegrees(7.00686, 46.66365),
-      Cesium.Cartesian3.fromDegrees(6.87359, 46.67890),
-      Cesium.Cartesian3.fromDegrees(6.84562, 46.65247),
-      Cesium.Cartesian3.fromDegrees(6.90941, 46.62848),
-      Cesium.Cartesian3.fromDegrees(6.94127, 46.61058),
-      Cesium.Cartesian3.fromDegrees(7.25892, 46.68031),
-      Cesium.Cartesian3.fromDegrees(7.26710, 46.69877),
-      Cesium.Cartesian3.fromDegrees(6.84192, 46.74431),
-      Cesium.Cartesian3.fromDegrees(7.11054, 46.84741),
-      Cesium.Cartesian3.fromDegrees(7.09383, 46.83535),
-    ];
+    // Ajoute les éoliennes
+    this.displayEolienne();
 
-    for (var i = 0; i < positions.length; i++) {
-      this.viewer.entities.add({
-        position: positions[i],
-//        model : {
-//          uri : "../../assets/scene.gltf",
-//          scale : 10,
-//        },
-        ellipse: {
-          semiMinorAxis: 5,
-          semiMajorAxis: 5,
-          heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-          extrudedHeight: 90,
-          material: Cesium.Color.AQUA,
-        },
-      })
-      this.viewer.entities.add({
-        position: positions[i],
-        ellipse: {
-          semiMinorAxis: 30,
-          semiMajorAxis: 30,
-          material: Cesium.Color.AQUA,
-        },
-      })
-    };
   },
 };
 </script>
