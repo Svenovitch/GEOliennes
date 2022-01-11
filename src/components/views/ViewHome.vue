382af30b87eb11413d44c91e77c8764d8aa59dc9
Sven Fragnière <sven.fragniere@gmail.com>
2022-01-11 12:38:15 +0100

    Page d'accueil

--- src/components/views/ViewHome.vue
+++ src/components/views/ViewHome.vue
@@ -1,11 +1,9 @@
 <template>
-  <p class="title is-3"> {{ hellomessage }} </p>
+  <p class="title is-3 notification is-dark is-light"> {{ hellomessage }} </p>
   <div class="tile is-ancestor">
     <div class="tile is-6 is-vertical">
-      <div class="tile is-parent">
-        <article class="tile is-child">
-          <p class="title is-4">Notre équipe</p>
-        </article>
+      <div class="content">
+        <p class="title is-3">Notre équipe</p>
       </div>
       <div class="tile is-12 notification is-info is-light">
         <div v-for="collaborateur in team" :key="collaborateur" class="tile is-4">
@@ -20,15 +18,19 @@
       </div>
     </div>
     <div class="tile is-6 is-vertical">
-      <div class="tile is-parent">
-        <article class="tile is-child">
-          <div class="content">
-            <p class="title is-4">Le Projet</p>
-            <div class="content">
-              <p class="is-small">dfsdgsfdfsfdhdfhfsdsfsfwgdwgdsgdsgdsgdsgdsgdsgdsgreefehehrfdgazre r zrezeqe  utuaet utau tr ur srewhbfhdsfv hvhjfvhsavf vfjsavfhufvh</p>
-            </div>
-          </div>
-        </article>
+      <div class="content">
+        <p class="title is-3">Le Projet</p>
+      </div>
+      <div class="tile is-child notification is-warning is-light">
+        <article class="content has-text-justified has-text-black">
+          <p>Les projets d’éoliennes font l’objet de nombreux débats. Ils sont souvent mal perçus par la population qui les rejettent presque systématiquement.</p>
+          <p>Il est parfois difficile de se représenter l’impact qu’ont de tels projets sur notre territoire.
+             Le but de ce géoportail est de fournir un support sur lequel représenter les éoliennes dans leur environnement, aussi bien sur une carte 2D qu’en 3 dimensions sur un globe virtuel.</p>
+          <p>L’affichage sur la carte 2D permet de visualiser les éoliennes sur divers fonds de plan (carte nationale et imagerie aérienne par exemple).
+             Pour chaque éolienne, un viewshed représentant les zones depuis lesquelles elles sont visibles peut être affiché.</p>
+          <p>L’affichage 3D permet lui de visualiser un globe virtuel sur lequel les éoliennes, les bâtiments et les arbres sont représentés.
+             Une fonction offre la possibilité à l’utilisateur de se projeter au sol, ce qui lui permet de s’immerger dans le paysage.</p>
+        </article> 
       </div>
     </div>
   </div>
